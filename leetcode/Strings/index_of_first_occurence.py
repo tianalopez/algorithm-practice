14aafbd37d152fc7559056e87c3eca217b389719
Tiana Lopez <lopez.y.tiana@gmail.com>
2024-03-01 20:13:03 -0800

    adds problem

---
+++ leetcode/Strings/index_of_first_occurence.py
@@ -0,0 +1,6 @@
+# Given two strings needle and haystack,
+# return the index of the first occurrence of needle
+# in haystack, or -1 if needle is not part of haystack.
+
+#!Difficulty: Easy
+#!Type: String
