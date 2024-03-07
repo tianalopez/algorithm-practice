bced5ce02183b645a656047e26860af3484e1f95
Tiana Lopez <lopez.y.tiana@gmail.com>
2024-03-07 14:14:25 -0800

    adds corrected solution

--- leetcode/Array/remove_array.py
+++ leetcode/Array/remove_array.py
@@ -9,7 +9,9 @@
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        for item in nums:
-            if item == val:
-                nums.remove(val)
-        return len(nums)
+        index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+                index += 1
+        return index
