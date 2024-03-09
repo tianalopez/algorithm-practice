863fb39c909ff2b4c11793c5a8deb7f963e93723
Tiana Lopez <lopez.y.tiana@gmail.com>
2024-03-09 11:23:44 -0800

    adds solution

--- leetcode/Array/get_common.py
+++ leetcode/Array/get_common.py
@@ -8,3 +8,19 @@
 
 #!Difficulty: Easy
 #!Category: Array
+
+
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        data = {}
+        for i in range(len(nums1)):
+            if nums1[i] not in data.keys():
+                data[nums1[i]] = 0
+        for j in range(len(nums2)):
+            if nums2[j] in data.keys():
+                data[nums2[j]] = 1
+        maximums = [num for num in data.keys() if data[num] == 1]
+        if len(maximums) >= 1:
+            return min(maximums)
+        else:
+            return -1
