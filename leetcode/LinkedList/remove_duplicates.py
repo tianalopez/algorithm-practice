b17263bef5fcccc216e9f7ee39e76755a51d1b6c
Tiana Lopez <lopez.y.tiana@gmail.com>
2024-03-15 22:35:44 -0700

    adds solution

--- leetcode/LinkedList/remove_duplicates.py
+++ leetcode/LinkedList/remove_duplicates.py
@@ -1,3 +1,14 @@
 # Given the head of a sorted linked list,
 # delete all duplicates such that each element appears only once.
 # Return the linked list sorted as well.
+
+
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        current = head
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return head
