38f60403cf8fe0a5e2fa561ac829ef156f8ac498
Tiana Lopez <lopez.y.tiana@gmail.com>
2024-03-20 22:26:07 -0700

    adds solution

--- leetcode/LinkedList/linked_list_cycle.py
+++ leetcode/LinkedList/linked_list_cycle.py
@@ -11,3 +11,15 @@
 
 #!Category: Linked List
 #!Difficulty: Easy
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        visited = set()
+        node = head
+        while node is not None:
+            if node in visited:
+                return True
+            visited.add(node)
+            node = node.next
+        return False
