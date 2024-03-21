5e213ef11b32f72f69e0525e456e42aab04d7721
Tiana Lopez <lopez.y.tiana@gmail.com>
2024-03-20 22:50:30 -0700

    adds solution

--- leetcode/LinkedList/reverse_linked_list.py
+++ leetcode/LinkedList/reverse_linked_list.py
@@ -1,2 +1,17 @@
 # Given the head of a singly linked list,
 # reverse the list, and return the reversed list.
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev_node = None
+        current_node = head
+
+        while current_node:
+            next_node = current_node.next
+            current_node.next = prev_node
+
+            prev_node = current_node
+            current_node = next_node
+
+        return prev_node
